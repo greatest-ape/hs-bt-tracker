4468501fcc7512a84fbf835aa498ff4e324f4d36
Joakim Frostegård <joakim.frostegard@gmail.com>
2016-12-29 01:13:38 +0100

    Don't export Convertors.Common in Convertors

--- app/Converters.hs
+++ app/Converters.hs
@@ -1,9 +1,7 @@
 module Converters (
-    module Converters.Common,
     module Converters.Requests,
     module Converters.Responses
 ) where
 
-import Converters.Common
 import Converters.Requests
 import Converters.Responses
