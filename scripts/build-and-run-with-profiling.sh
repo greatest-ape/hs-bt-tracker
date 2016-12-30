dd5f793c0d9d60787ffeb8a3f5076234cda36e69
Joakim Frostegård <joakim.frostegard@gmail.com>
2016-12-30 03:14:33 +0100

    Add scripts for building and executing the binary
    Both with and without profiling

---
+++ scripts/build-and-run-with-profiling.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/32123475/profiling-builds-with-stack
+stack build --executable-profiling --library-profiling --ghc-options "-O -fprof-auto -rtsopts" &&
+    stack exec -- hs-bt-tracker +RTS -N -p
